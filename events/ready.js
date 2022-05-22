2165bd8418861e2e7d0341937fbc089f2188f7de
Vijay-raj12 <97966081+Vijay-raj12@users.noreply.github.com>
2022-05-22 08:53:20 +0530

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,51 @@
+///////////////////////////////////
+
+//💙 Credit Infos 
+//💜 This Project Made By Aditya Codez
+//💛 Must Give Credits While Using 
+//💚 Support Server 
+//♥️ https://discord.gg/z6RMrphPXE
+//🔥 Youtube 
+//🙉 https://youtube.com/adityacodez
+//🏘️ ©Aditya Codez™
+
+///////////////////////////////////
+
+const chalk = require('chalk');
+const rpc = require("discordrpcgenerator");
+const config = require(".././config.json")
+
+module.exports = async(client) => {
+  const small = await rpc.getRpcImage(config.applicationid, config.smallimgname);
+  
+  rpc.getRpcImage(config.applicationid, config.imagename)
+  .then(large => {
+    let presence = new rpc.Rpc()
+    .setName(config.name)
+    .setType(config.type)
+    .setApplicationId(config.applicationid)
+    .setAssetsLargeImage(large.id)
+
+.setAssetsSmallImage(small.id)   
+      .setAssetsLargeText(large.name)
+    .setStartTimestamp(config.time || Date.now())
+    console.log(presence.toDiscord())
+    client.user.setStatus("dnd");
+    client.user.setPresence(presence.toDiscord()).catch(console.error);
+  })
+  console.log(chalk.hex("#ff0000")("Succesfully enabled rpc"))
+           }
+
+
+///////////////////////////////////
+
+//💙 Credit Infos 
+//💜 This Project Made By Aditya Codez
+//💛 Must Give Credits While Using 
+//💚 Support Server 
+//♥️ https://discord.gg/z6RMrphPXE
+//🔥 Youtube 
+//🙉 https://youtube.com/adityacodez
+//🏘️ ©Aditya Codez™
+
+///////////////////////////////////
